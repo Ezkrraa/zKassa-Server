aa3629d5402f2a352fb6682db7145198822e035f
Ezkrraa <ezravdkolk@outlook.com>
2025-01-21 19:30:23 +0100

    Add support for quantity in barcodes in format of x*yyyy for a quantity of x using y barcode

--- zKassa-Server/ControllerModels/ProductInfo.cs
+++ zKassa-Server/ControllerModels/ProductInfo.cs
@@ -7,12 +7,14 @@ namespace zKassa_Server.ControllerModels
         public Guid Id { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
+        public int Quantity { get; set; }
 
-        public ProductInfo(Product product)
+        public ProductInfo(Product product, int quantity)
         {
             Id = product.Id; 
             Name = product.Name;
             Price = product.Price;
+            Quantity = quantity;
         }
     }
 }
