380a40d99db26387a8eeaf56a03397140ef4698f
Ezkrraa <ezravdkolk@outlook.com>
2025-01-21 19:15:58 +0100

    Update loginModel and use extra fields for password check

--- zKassa-Server/Controllers/AuthenticationController.cs
+++ zKassa-Server/Controllers/AuthenticationController.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using zKassa_Server.ControllerModels;
@@ -12,17 +13,20 @@ public class AuthenticationController : ControllerBase
 {
     private readonly ILogger<AuthenticationController> _logger;
     private readonly UserManager<Employee> _userManager;
+    private readonly SignInManager<Employee> _authManager;
     private readonly JwtService _jwtService;
     private readonly IConfiguration _configuration;
 
     public AuthenticationController(
         ILogger<AuthenticationController> logger,
+        SignInManager<Employee> authManager,
         UserManager<Employee> userManager,
-        JwtService jwtService,
+    JwtService jwtService,
         IConfiguration configuration
     )
     {
         _logger = logger;
+        _authManager = authManager;
         _userManager = userManager;
         _jwtService = jwtService;
         _configuration = configuration;
@@ -31,10 +35,14 @@ public class AuthenticationController : ControllerBase
     [HttpPost("Login")]
     public async Task<IActionResult> Login([FromBody] LoginModel loginModel)
     {
+        
         Employee? user = await _userManager.FindByNameAsync(loginModel.UserName);
         if (user == null)
             return NotFound("No such user");
         // TODO: check if user is permitted to use their drawer on that day etc.
+        Microsoft.AspNetCore.Identity.SignInResult? signinResult = await _authManager.PasswordSignInAsync(user, loginModel.Password, true, false);
+        if (signinResult == null || !signinResult.Succeeded)
+            return Unauthorized("Invalid password");
         return Ok(new LoginResponse(_jwtService.GenerateToken(user, _configuration), user.Role));
     }
 }
